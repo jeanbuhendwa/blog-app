248273cec7a1dc77249de46ec093ab9c3e762f07
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-23 00:38:51 +0300

    Add home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Post from "./components/Post/Post";
 
 function App() {
   return (
     <main className="main__container">
       <Header />
+      <Post />
     </main>
   );
 }
