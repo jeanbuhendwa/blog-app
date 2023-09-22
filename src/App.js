3271d09db130958aaaf76faa0d35b5a96f11c216
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-22 14:36:41 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <>
-      <h1>Hello Blog</h1>
-    </>
+    <main className="main__container">
+      <Header />
+    </main>
   );
 }
 
