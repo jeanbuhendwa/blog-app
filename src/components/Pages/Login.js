fff6452392fdc8e68fe6f4a716334655ca03bbe6
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-24 23:20:57 +0300

    Add pages

---
+++ src/components/Pages/Login.js
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <h1 className="login">Login</h1>
+    </>
+  );
+};
+
+export default Login;
---
+++ src/components/Pages/Register.js
@@ -0,0 +1,9 @@
+const Register = () => {
+  return (
+    <>
+      <h1 className="login">Register</h1>
+    </>
+  );
+};
+
+export default Register;
