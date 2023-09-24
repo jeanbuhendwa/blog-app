14de919532ff7b4ebe09cdb9ebf1faa76838c1c0
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-24 23:20:05 +0300

    Add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./components/Header/Header";
 import Post from "./components/Post/Post";
+import Layout from "./components/Routes/Layout";
+import Login from "./components/Pages/Login";
+import Register from "./components/Pages/Register";
 
 function App() {
   return (
-    <main className="main__container">
-      <Header />
-      <Post />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/regester" element={<Register />} />
+      </Route>
+    </Routes>
   );
 }
 
