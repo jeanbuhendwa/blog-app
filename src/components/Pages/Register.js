595477b9d36c17aa027c15ac42f07380f49647b9
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-25 16:27:01 +0300

    Update register js

--- src/components/Pages/Register.js
+++ src/components/Pages/Register.js
@@ -1,8 +1,15 @@
 const Register = () => {
   return (
-    <>
-      <h1 className="login">Register</h1>
-    </>
+    <section className="form section">
+      <form className="form__container">
+        <h2 className="form__title">Register</h2>
+        <div className="form__group">
+          <input className="input" type="text" placeholder="Email" />
+          <input className="input" type="password" placeholder="Password" />
+          <button className="btn">Register</button>
+        </div>
+      </form>
+    </section>
   );
 };
 
