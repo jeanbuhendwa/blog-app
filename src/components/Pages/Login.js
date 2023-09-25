39dd149903c10a97cc438637b3aa9dabbb21192a
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-25 16:26:23 +0300

    Update login js

--- src/components/Pages/Login.js
+++ src/components/Pages/Login.js
@@ -1,8 +1,17 @@
+import "./form.css";
+
 const Login = () => {
   return (
-    <>
-      <h1 className="login">Login</h1>
-    </>
+    <section className="form section">
+      <form className="form__container">
+        <h2 className="form__title">Login</h2>
+        <div className="form__group">
+          <input className="input" type="text" placeholder="Email" />
+          <input className="input" type="password" placeholder="Password" />
+          <button className="btn">Login</button>
+        </div>
+      </form>
+    </section>
   );
 };
 
