899b843dd33e02997836da9c85084549c44bc6ef
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-26 13:52:08 +0300

    add autentication

--- src/components/Pages/Register.js
+++ src/components/Pages/Register.js
@@ -1,11 +1,45 @@
+import { useState } from "react";
+
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function register(e) {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/register", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.status === 200) {
+      alert("Successfully registered!");
+    } else {
+      alert("Something went wrong!");
+    }
+  }
   return (
     <section className="form section">
-      <form className="form__container">
+      <form className="form__container" onSubmit={register}>
         <h2 className="form__title">Register</h2>
         <div className="form__group">
-          <input className="input" type="text" placeholder="Email" />
-          <input className="input" type="password" placeholder="Password" />
+          <input
+            className="input"
+            type="text"
+            placeholder="Email"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="input"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button className="btn">Register</button>
         </div>
       </form>
