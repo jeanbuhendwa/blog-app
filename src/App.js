6a7325d268344efde94e9098b467bfd61883654a
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-28 21:51:28 +0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Register from "./components/Pages/Register";
 import { UserContextProvider } from "./components/UserContext";
 import CreatePost from "./components/Pages/post/createPost";
 import IndexPage from "./components/indexPage";
+import PostPage from "./components/Pages/postPage/postPage";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/regester" element={<Register />} />
           <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
