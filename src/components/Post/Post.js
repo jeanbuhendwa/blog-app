e93e27886c2936c42c441d9e3a790993493fa362
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-28 15:39:17 +0300

    Fetch post from DB

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,143 +1,20 @@
+import { format } from "date-fns";
 import "./post.css";
-const Post = () => {
+const Post = ({ title, summary, content, cover, createdAt, author }) => {
   return (
-    <section className="post__container section">
-      <div className="post">
-        <div className="post__image">
-          <img
-            src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
-            alt=""
-          />
-        </div>
-        <div className="post__content">
-          <h2>The Art of Mindful Living</h2>
-          <p className="post__date">
-            <a className="author">John Buh</a>
-            <time>2023-09-01 11:00</time>
-          </p>
-          <p>
-            In today's fast-paced world, it's easy to get caught up in the
-            hustle and bustle of daily life. From juggling work responsibilities
-            to managing household chores and social commitments, it often feels
-            like there's no time to pause and catch our breath. This constant
-            rush can take a toll on our mental and physical well-being. But
-            amidst the chaos, there exists a profound practice that can help us
-            find inner peace and restore balance to our lives - mindfulness. The
-            art of mindful living is a transformative journey that allows us to
-            be fully present in each moment, appreciating the beauty of life as
-            it unfolds.
-          </p>
-        </div>
+    <div className="post">
+      <div className="post__image">
+        <img src={"http://localhost:5000/" + cover} alt="Img Bg Post" />
       </div>
-      <div className="post">
-        <div className="post__image">
-          <img
-            src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
-            alt=""
-          />
-        </div>
-        <div className="post__content">
-          <h2>The Art of Mindful Living</h2>
-          <p className="post__date">
-            <a className="author">John Buh</a>
-            <time>2023-09-01 11:00</time>
-          </p>
-          <p>
-            In today's fast-paced world, it's easy to get caught up in the
-            hustle and bustle of daily life. From juggling work responsibilities
-            to managing household chores and social commitments, it often feels
-            like there's no time to pause and catch our breath. This constant
-            rush can take a toll on our mental and physical well-being. But
-            amidst the chaos, there exists a profound practice that can help us
-            find inner peace and restore balance to our lives - mindfulness. The
-            art of mindful living is a transformative journey that allows us to
-            be fully present in each moment, appreciating the beauty of life as
-            it unfolds.
-          </p>
-        </div>
+      <div className="post__content">
+        <h2>{title}</h2>
+        <p className="post__date">
+          <a className="author">{author.username}</a>
+          <time>{format(new Date(createdAt), "MMMM dd, yyyy h:mm a")}</time>
+        </p>
+        <p>{summary}</p>
       </div>
-      <div className="post">
-        <div className="post__image">
-          <img
-            src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
-            alt=""
-          />
-        </div>
-        <div className="post__content">
-          <h2>The Art of Mindful Living</h2>
-          <p className="post__date">
-            <a className="author">John Buh</a>
-            <time>2023-09-01 11:00</time>
-          </p>
-          <p>
-            In today's fast-paced world, it's easy to get caught up in the
-            hustle and bustle of daily life. From juggling work responsibilities
-            to managing household chores and social commitments, it often feels
-            like there's no time to pause and catch our breath. This constant
-            rush can take a toll on our mental and physical well-being. But
-            amidst the chaos, there exists a profound practice that can help us
-            find inner peace and restore balance to our lives - mindfulness. The
-            art of mindful living is a transformative journey that allows us to
-            be fully present in each moment, appreciating the beauty of life as
-            it unfolds.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="post__image">
-          <img
-            src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
-            alt=""
-          />
-        </div>
-        <div className="post__content">
-          <h2>The Art of Mindful Living</h2>
-          <p className="post__date">
-            <a className="author">John Buh</a>
-            <time>2023-09-01 11:00</time>
-          </p>
-          <p>
-            In today's fast-paced world, it's easy to get caught up in the
-            hustle and bustle of daily life. From juggling work responsibilities
-            to managing household chores and social commitments, it often feels
-            like there's no time to pause and catch our breath. This constant
-            rush can take a toll on our mental and physical well-being. But
-            amidst the chaos, there exists a profound practice that can help us
-            find inner peace and restore balance to our lives - mindfulness. The
-            art of mindful living is a transformative journey that allows us to
-            be fully present in each moment, appreciating the beauty of life as
-            it unfolds.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="post__image">
-          <img
-            src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
-            alt=""
-          />
-        </div>
-        <div className="post__content">
-          <h2>The Art of Mindful Living</h2>
-          <p className="post__date">
-            <a className="author">John Buh</a>
-            <time>2023-09-01 11:00</time>
-          </p>
-          <p>
-            In today's fast-paced world, it's easy to get caught up in the
-            hustle and bustle of daily life. From juggling work responsibilities
-            to managing household chores and social commitments, it often feels
-            like there's no time to pause and catch our breath. This constant
-            rush can take a toll on our mental and physical well-being. But
-            amidst the chaos, there exists a profound practice that can help us
-            find inner peace and restore balance to our lives - mindfulness. The
-            art of mindful living is a transformative journey that allows us to
-            be fully present in each moment, appreciating the beauty of life as
-            it unfolds.
-          </p>
-        </div>
-      </div>
-    </section>
+    </div>
   );
 };
 
