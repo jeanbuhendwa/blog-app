a9487dc007732827a13ed54cb4fc579755a1f964
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-28 15:38:17 +0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Post from "./components/Post/Post";
 import Layout from "./components/Routes/Layout";
 import Login from "./components/Pages/Login";
 import Register from "./components/Pages/Register";
 import { UserContextProvider } from "./components/UserContext";
 import CreatePost from "./components/Pages/post/createPost";
+import IndexPage from "./components/indexPage";
 
 function App() {
   return (
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Post />} />
+          <Route index element={<IndexPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/regester" element={<Register />} />
           <Route path="/create" element={<CreatePost />} />
