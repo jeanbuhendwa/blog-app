56fb2d903150edda112437d4c94b4966d73a9a2d
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-10-03 13:44:47 +0300

    Add edit post

---
+++ src/components/Pages/editPost/EditPost.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { Navigate, useParams } from "react-router-dom";
+import Editor from "../../Editor";
+
+const EditPost = () => {
+  const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+  const [redirect, setRedirect] = useState(false);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/post/${id}`).then((response) => {
+      response.json().then((postInfo) => {
+        setTitle(postInfo.title);
+        setSummary(postInfo.summary);
+        setContent(postInfo.content);
+        setFiles(postInfo.file);
+      });
+    });
+  }, []);
+  const updatePost = async (ev) => {
+    ev.preventDefault();
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files?.[0]);
+    }
+    const response = await fetch(`http://localhost:5000/post/`, {
+      method: "PUT",
+      body: data,
+      credentials: "include",
+    });
+    if (response.ok) {
+      setRedirect(true);
+    }
+  };
+
+  if (redirect) {
+    return <Navigate to={"/post/" + id} />;
+  }
+  return (
+    <section className="post__container section">
+      <form className="form_container" onSubmit={updatePost}>
+        <input
+          className="input_post"
+          type="title"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          className="input_post"
+          type="summary"
+          placeholder="Summary"
+          value={summary}
+          onChange={(e) => setSummary(e.target.value)}
+        />
+        <input
+          className="input_post"
+          type="file"
+          onChange={(e) => setFiles(e.target.files)}
+        />
+        <Editor value={content} onChange={setContent} />
+        <button className="btn btn__post">Update Post</button>
+      </form>
+    </section>
+  );
+};
+
+export default EditPost;
